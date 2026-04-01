4b1d693bea98ccf47c33225c9686c25b03b9d5b8
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 18:11:05 +0530

    date

--- 11-condition.sh
+++ 11-condition.sh
@@ -2,9 +2,19 @@
 
 #given numbr even or odd
 number=$1
-if [ $number/2 -eq 0 ];
+if [ $((number % 2)) -eq 0 ];
 then
     echo "$number is even number"
 else
-    cho "$number is odd number"
+    echo "$number is odd number"
+fi
+
+#or
+num1=$1
+num2=$((num1/2))
+if [ $num2 -eq 0 ];
+then
+    echo "$num1 is even number"
+else
+    echo "$number is odd number"
 fi
