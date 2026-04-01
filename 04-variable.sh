832af46af13f0fab43095a93e60fc418a1b98748
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 13:58:19 +0530

    add argument variable

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: welcome to india $person1"
+echo "$person2:: Thank you $person1,Fnally not on zoom webniar"
+echo "$person1:: so, do we have deal"
+echo "$person2:: yes, if u make"
