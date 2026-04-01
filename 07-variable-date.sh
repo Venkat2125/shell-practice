f6010def07ceb42a5dd04b1eeb58c04fdb08c994
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 16:30:57 +0530

    date

--- 07-variable-date.sh
+++ 07-variable-date.sh
@@ -4,5 +4,5 @@ echo "today date with sec $today"
 day=$(date +%y-%m-%d-%h-%M-%S)
 sleep 25
 end_time=$(date +%y-%m-%d-%h-%M-%S)
-total_time=$(($day-$end_tme))
+total_time=$(($day-$end_time))
 echo "total time $total_time"
