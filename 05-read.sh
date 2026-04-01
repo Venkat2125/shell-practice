1808d5897512e0f8bdea84c23362112155dc1f36
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 14:08:29 +0530

    read

--- 05-read.sh
+++ 05-read.sh
@@ -8,3 +8,14 @@ echo "your number is $number"
 echo "Enter your PIN"
 read PIN_NUMBER
 echo "your pin number is $PIN_NUMBER"
+
+#output:
+#Enter your PIN
+#1234
+#your pin number is 1234
+echo "Now we enter ping agian we add read -s"
+#above output i't showng your pin. but we don't need to see anyone. if we login linux server we enter pin did we see? No
+#use read -s "it will not visible your pin"
+echo "Enter your PIN"
+read -s PIN_NUMBER
+echo "your pin number is $PIN_NUMBER"
