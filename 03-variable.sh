45e423955ffca097121a96a9f5854e06b140a6d8
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 13:11:57 +0530

    add variable

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#no space before or after equal
+person1="chitti"
+person2="lakshmi"
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you?"
+echo "$person1:: what are you learnng recently $person2?"
+echo "$person2:: im learning devops $person1"
