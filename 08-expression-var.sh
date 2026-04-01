44450e11ffbbb442e898b09ccec0279a7f3030ec
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 16:42:59 +0530

    date

---
+++ 08-expression-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#expression variables
+echo "each argument treat seprate: $@"
+echo "all arguments one string $*"
+echo " pid of this script $$"
+echo "pid of last command in the script $!"
+echo "script name $0"
+echo "who is running this $USER"
+echo "Home directory $HOME"
+echo "present working drectory $PWD"
