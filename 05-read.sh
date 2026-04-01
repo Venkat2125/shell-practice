8686c69b97d11e9cacb480189f671ec6ec7b65b3
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 14:05:32 +0530

    read

--- 05-read.sh
+++ 05-read.sh
@@ -3,3 +3,8 @@
 echo "Enter your number"
 read number
 echo "your number is $number"
+
+# Now you want to set your ATM pin
+echo "Enter your PIN"
+read PIN_NUMBER
+echo "your pin number is $PIN_NUMBER"
