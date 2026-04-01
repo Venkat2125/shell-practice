4b742a7b8bd9e70bd5f6a7a0675c42faccb94863
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 12:58:12 +0530

    some convesation

---
+++ 02-convesation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "venkat:: Hi Shivanna, How are you?"
+echo "shivanna:: Hello venkat, I am fine. How are you?"
+echo "venkat:: what are yu learnng recently?"
+echo "shivanna:: im learning devops"
