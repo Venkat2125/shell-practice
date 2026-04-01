91b0d404d0981158a867df787b11b830df57d12d
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 14:23:48 +0530

    read

--- 06-variable-env.sh
+++ 06-variable-env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+#using environment varibale we crete export into live server. if we want thisone complete whwener run server as we can add on .bash
+#.bash file
 echo "my name is $name"
