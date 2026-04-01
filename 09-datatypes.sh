955706448c6311630d16f21f15548c4fbdf55945
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-01 17:07:59 +0530

    date

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -7,6 +7,13 @@ z="venkat"
 total=$(($x+$y+$z))
 echo "total of x,y,z =${total}"
 
-#variables
+#arrays
 name=("venkat" "ammu" "sai" "chitti" "umesh" "puru")
 echo "all names: ${name[@]}"
+
+#index
+city=("gvp" "kdr" "wrngl" "hyd" "bnglr" "tpt" "TN" "ap" "TS")
+echo " city list ${city[@]}"
+echo "index of fourth city: ${city[3]}"
+echo "second city: third city: ${city[6]}"
+echo "last city: ${city[12]}"
