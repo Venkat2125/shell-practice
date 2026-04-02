ceacc1b071e78b753de691ddf6b9e734495f6063
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-02 12:12:00 +0530

    install mysql

---
+++ 12-installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# check if user has root access or not
+user_id=$(id -u)  #id -u will check root access
+
+if [ $user_id -ne 0 ];
+then
+    echo " user has doesn't root access,please use root user"
+else
+    echo "sudo user logged in"
+fi
+dnf install mysql-server -y
+
+#how to we know its success or failed? use $? it will tells last command status
+if [ $? -eq 0 ];
+then
+    echo "installed successfully"
+else
+    echo "failed check logs"
+fi
+
+#install mysql
