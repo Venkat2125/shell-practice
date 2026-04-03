1cf87461548b42f2fb391e8b5ab4c29198758fc5
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-03 14:45:48 +0530

    installing mysql

--- 13-installation.sh
+++ 13-installation.sh
@@ -14,6 +14,23 @@ dnf install mysql-server -y
 if [ $? -eq 0 ];
 then
     echo "installed successfully"
+
 else
     echo "installed failed"
+fi
+echo "check user"
+check_user=$(id -u)
+if [ $check_user -eq 0 ];
+then
+    echo "logged in sudo user"
+else
+    echo "it's doesnt have access, login sudo user"
+fi
+dnf install nginx -y
+download=$?
+if [ $download -eq 0 ];
+then
+    echo "nginx successfully installed"
+else
+    echo "nginx download failed"
 fi
