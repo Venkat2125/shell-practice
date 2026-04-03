eb25679c468a0aa5b18206bd9c2ea71f73e03880
Venkat2125 <venkatsavi2125@gmail.com>
2026-04-03 14:34:41 +0530

    installing mysql

---
+++ 13-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#install mysql, nginx, python
+
+user_id=$(id -u)
+
+if [ $user_id -ne 0 ];
+then
+    echo "logged failed"
+else
+    echo "logged successfully"
+fi
+dnf install mysql-server -y
+if [ $? -eq 0 ];
+then
+    echo "installed successfully"
+else
+    echo "installed failed"
+fi
